1034c459d6a39487db664bfe8ca00a8f6a366223
pertsevpv <pertsevpv@yandex.ru>
2022-04-29 13:36:15 +0300

    Some UI changes

--- src/main/java/enhanced/search/service/SearchService.kt
+++ src/main/java/enhanced/search/service/SearchService.kt
@@ -9,31 +9,33 @@ import org.springframework.stereotype.Service
 @Service("searchService")
 class SearchService {
 
-    fun getGroups() = listOf(
-        Group(1, "Group 1"),
-        Group(2, "Group 2"),
-        Group(3, "Group 3"),
-        Group(4, "Group 4"),
-    )
-
     fun getGroupTypes() = listOf(
         GroupType(1, "Group Type 1"),
-        GroupType(2, "Group Type 2"),
-        GroupType(3, "Group Type 3"),
-        GroupType(4, "Group Type 4"),
+        GroupType(2, "Group Type 2")
     )
 
+    fun getGroups() = listOf(
+        Group(1, "Group 1", 1),
+        Group(2, "Group 2", 1),
+        Group(3, "Group 3", 1),
+        Group(4, "Group 4", 2),
+    )
+
+
     fun getRepositories() = listOf(
-        Repository(1, "Repository 1"),
-        Repository(2, "Repository 2"),
-        Repository(3, "Repository 3"),
-        Repository(4, "Repository 4"),
+        Repository(1, "Repository 1", 1),
+        Repository(2, "Repository 2", 2),
+        Repository(3, "Repository 3", 3),
+        Repository(4, "Repository 4", 3),
+        Repository(5, "Repository 5", 4),
     )
 
     fun getBranches() = listOf(
-        Branch(1, "Branch 1"),
-        Branch(2, "Branch 2"),
-        Branch(3, "Branch 3"),
-        Branch(4, "Branch 4"),
+        Branch(1, "Branch 1", 1),
+        Branch(2, "Branch 2", 1),
+        Branch(3, "Branch 3", 2),
+        Branch(4, "Branch 4", 3),
+        Branch(5, "Branch 5", 4),
+        Branch(6, "Branch 6", 5),
     )
 }
--- src/main/java/enhanced/search/ui/SearchController.kt
+++ src/main/java/enhanced/search/ui/SearchController.kt
@@ -2,21 +2,23 @@ package enhanced.search.ui
 
 import enhanced.search.dto.*
 import enhanced.search.service.SearchService
+import enhanced.search.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
+import java.security.Principal
 
 @Controller
 class SearchController(
-    @Autowired private val searchService: SearchService
+    @Autowired private val searchService: SearchService,
+    @Autowired private val userService: UserService
 ) {
 
     @GetMapping("/")
     fun search(model: Model): String {
-
         model.addAttribute("request", SearchRequest())
         model.addAttribute("groupList", listOf(ANY_GROUP).plus(searchService.getGroups()))
         model.addAttribute("groupTypeList", listOf(ANY_GROUP_TYPE).plus(searchService.getGroupTypes()))
@@ -31,6 +33,8 @@ class SearchController(
         @ModelAttribute gotRequest: SearchRequest,
         model: Model
     ): String {
+        println(gotRequest.toString())
+
         model.addAttribute("gotRequest", gotRequest)
 
         model.addAttribute("request", SearchRequest())
@@ -38,10 +42,6 @@ class SearchController(
         model.addAttribute("groupTypeList", listOf(ANY_GROUP_TYPE).plus(searchService.getGroupTypes()))
         model.addAttribute("repositoryList", listOf(ANY_REPOSITORY).plus(searchService.getRepositories()))
         model.addAttribute("branchList", searchService.getBranches())
-
-        println(gotRequest)
-        model.addAttribute("request", gotRequest)
-        println(gotRequest.toString())
 
         return "search-results"
     }
